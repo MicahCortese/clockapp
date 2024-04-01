752f3817678671cb05343a33d4681717a55b0dfe
corcheesey <micahcortese@gmail.com>
2024-04-01 19:54:55 -0400

    Implemented a TextClock to display current time from the device. Implemented a TextDate to display current date in Month-Day-Year format above the TextClock.

--- app/src/main/java/com/zybooks/clockapp/MainActivity.java
+++ app/src/main/java/com/zybooks/clockapp/MainActivity.java
@@ -1,24 +1,30 @@
 package com.zybooks.clockapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Create calendar object, grab current time, and implement date format Month-Day-Year.
+        textDate = (TextView) findViewById(R.id.editTextDate);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM - dd - yyyy");
+        String dateTime = dateFormat.format(calendar.getTime()).toString();
+        textDate.setText(dateTime);
     }
 }
+
