fcfd614fe3273e57f995fa3e345869e7b6ef30b0
corcheesey <micahcortese@gmail.com>
2024-04-19 13:52:36 -0400

    Implemented timer fragment that starts from 1 hour and counts down to zero. Created a button on activity_main.xml, that when pressed, takes the user to the timer fragment and begins counting.

--- app/src/main/java/com/zybooks/clockapp/MainActivity.java
+++ app/src/main/java/com/zybooks/clockapp/MainActivity.java
@@ -12,15 +12,13 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textDate;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Create calendar object, grab current time and date, and implement date format "Day, Month Day(numerical)".
-        textDate = (TextView) findViewById(R.id.editTextDate);
+        TextView textDate = findViewById(R.id.editTextDate);
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMM d");
         String dateTime = dateFormat.format(calendar.getTime()).toString();
--- app/src/main/java/com/zybooks/clockapp/Timer.java
+++ app/src/main/java/com/zybooks/clockapp/Timer.java
@@ -5,49 +5,48 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Timer extends AppCompatActivity{
+public class Timer extends AppCompatActivity {
+    private TextView countdownTimer;
+    private CountDownTimer timer;
     private long timeLeftInSecond = 3600000;
-    //private TextView countdownTimer;
-    //private CountDownTimer timerObject;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-    }
-    TextView countdownTimer = findViewById(R.id.timer); //this line shows the textview from the activity_main.xml
 
-    CountDownTimer timer = new CountDownTimer(timeLeftInSecond, 1000) {
-        @Override
-        public void onTick(long secondUntilFinished) {
-            timeLeftInSecond = secondUntilFinished;
-            updateCountdownText();
-        }
+        countdownTimer = findViewById(R.id.timer); //this line shows the textview from the activity_timer.xml
 
-        @Override
-        public void onFinish() {
-            // Timer finishes, do something here
-        }
-    }.start();
+        timer = new CountDownTimer(timeLeftInSecond, 1000) {
+            @Override
+            public void onTick(long secondUntilFinished) {
+                timeLeftInSecond = secondUntilFinished;
+                updateCountdownText();
+            }
 
-    public Timer() {
+            @Override
+            public void onFinish() {
+                // Timer finishes, do something here
+            }
+        }.start();
     }
 
     private void updateCountdownText() {
-    int hours = (int) (timeLeftInSecond / 1000) / 3600;
-    int minutes = (int) ((timeLeftInSecond / 1000) % 3600) / 60;
-    int seconds = (int) (timeLeftInSecond / 1000) % 60;
+        int hours = (int) (timeLeftInSecond / 1000) / 3600;
+        int minutes = (int) ((timeLeftInSecond / 1000) % 3600) / 60;
+        int seconds = (int) (timeLeftInSecond / 1000) % 60;
 
 
 
-    String timeLeftFormatted = String.format("%02d:%02d:%02d", hours, minutes,seconds);
-    countdownTimer.setText(timeLeftFormatted);
-}
-
-@Override
-protected void onDestroy() {
-    super.onDestroy();
-    if (timer != null) {
-        timer.cancel();
+        String timeLeftFormatted = String.format("%02d:%02d:%02d", hours, minutes,seconds);
+        countdownTimer.setText(timeLeftFormatted);
     }
-  }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
 }
